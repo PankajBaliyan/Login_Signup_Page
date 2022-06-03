c1e77ec1ae6980bf40b332e1b1a0cc22435b7609
pankajkumar90 <pankajbaliyan90@gmail.com>
2022-06-03 08:42:10 +0530

    First Init

---
+++ database.php
@@ -0,0 +1,11 @@
+<?php
+
+$FirstName=$_POST['Name'];
+$Email=$_POST['Email'];
+$Stream=$_POST['Stream'];
+$Password=$_POST['Password'];
+
+    echo "Name: $FirstName <br>";
+    echo "Email: $Email <br>";
+    echo "Stream: $Stream <br>";
+    echo "Password: $Password <br>";
